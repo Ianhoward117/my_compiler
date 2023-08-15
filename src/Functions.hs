a117eeedf836c6299bc5c7541b7226b4214f5701
Ian Howard <ianlhoward2016@utexas.edu>
2023-08-14 19:49:29 -0500

    Not sure what I'm committing here

--- src/Functions.hs
+++ src/Functions.hs
@@ -1,4 +1,4 @@
-module Functions (double, quadruple, factorial, average, n, last_new2, initNew, second, swap, pair, palindrome, twice, quadrupleNew, product1, qsortRev, qsort2, doubleTwice, tail_new, init_new, init_new2, bools, nums, add, copy, apply, second_2, swap_2, pair_2, double_2, palindrome_2, twice_2) where
+module Functions (double, quadruple, factorial, average, n, last_new2, initNew, second, swap, pair, palindrome, twice, quadrupleNew, product1, qsortRev, qsort2, doubleTwice, tail_new, init_new, init_new2, bools, nums, add, copy, apply, second_2, swap_2, pair_2, double_2, palindrome_2, twice_2, safetail, safetail_guarded, safetail_matching) where
 
 -- FP4
 
@@ -159,7 +159,7 @@ chapter 3
     apply :: (a -> b) -> a -> b
     apply f x = f x
 
-        -- (3)
+--      (3)
 
     second_2 :: [a] -> a
     second_2 xs = head (tail xs)
@@ -179,9 +179,140 @@ chapter 3
     twice_2 :: (t -> t) -> t -> t
     twice_2 f x = f (f x)
 {-
-        -- (5)
+        (5)
 
-            -- In general, it is not feasible for function types to be instances of the Eq class because functions can not be compared for equality.
+            In general, it is not feasible for function types to be instances of the Eq class because functions can not be compared for equality.
 
-            -- It is feasible for function types to be instances of the Eq class when comparing a function to itself.
+            It is feasible for function types to be instances of the Eq class when comparing a function to itself.
+
+    FP6
+
+        Conditional Expressions
+
+            abs :: Int -> Int
+            abs n = if n >= 0 then n else -n
+
+            signum :: Int -> Int
+            signum n = if n < 0 then -1 else
+                            if n == 0 then 0 else 1
+
+            abs n | n >= 0 = n
+                  | otherwise = -n
+
+            signum n | n < 0 = -1
+                     | n == 0 = 0
+                     | otherwise = 1
+
+        Pattern Matching
+        
+            not :: Bool -> Bool
+            not False = True
+            not True = False
+
+            (&&) :: Bool -> Bool -> Bool
+            True && True = True
+            True && False = False
+            False && True = False
+            False && False = False
+
+            (&&) :: Bool -> Bool -> Bool
+            True && True = True
+            _ && _ = False
+
+            (&&) :: Bool -> Bool -> Bool
+            False && _ = False
+            True && a = a
+        
+        List Patterns
+
+            head :: [a] -> a
+            head (x:_) = x
+
+            tail :: [a] -> [a]
+            tail (_:xs) = xs
+
+        Lambda Expressions
+
+            \x = x + x
+
+            add :: Int -> Int -> Int
+            add x y = x + y
+            
+            add :: Int -> (Int -> Int)
+            add = \x -> (\y -> x + y)
+
+            odds n = map f [0..n-1]
+                where
+                    f x = x*2 + 1
+
+            odds n = map (\x -> x*2 + 1) [0..n-1]
+
+        Operator Sections
+
+            1+2 = 3
+            (+) 1 2 = 3
+
+            (1+) 2 = 3
+            (+2) 1 = 3
+
+            (1+) - successor function
+            (1/) - reciprocation function
+            (*2) - doubling function
+            (/2) - halving function
+
+        Exercises
+
+            (1)
+                (a)
+-}
+    safetail :: [a] -> [a]
+    safetail xs = if null xs then [] else tail xs
+
+--              (b)
+
+    safetail_guarded :: [a] -> [a]
+    safetail_guarded xs | null xs = []
+                        | otherwise = tail xs
+
+--              (c)
+
+    safetail_matching :: [a] -> [a]
+    safetail_matching [] = []
+    safetail_matching (_:xs) = xs
+
+{-          (2)
+
+    (||) :: Bool -> Bool -> Bool
+    False || False = False
+    False || True = True
+    True || False = True
+    True || True = True
+
+    (||) :: Bool -> Bool -> Bool
+    False || False = False
+    _ || _ = True
+
+    (||) :: Bool -> Bool -> Bool
+    _ || True = True
+    False || a = a
+
+            (3)
+
+    (&&) :: Bool -> Bool -> Bool
+    True && True = True
+    _    && _    = False
+    
+    (&&) :: Bool -> Bool -> Bool
+    a && b = if a == True then
+                if b == True then True else False
+             else False
+
+            (4)
+
+    (&&) :: Bool -> Bool -> Bool
+    True && b = b
+    False && _ = False
+
+    (&&) :: Bool -> Bool -> Bool
+    a && b = if a == True then b else False
 -}
